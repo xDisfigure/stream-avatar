e8cbcc4b094f44dd0b39192cbef5115e3164eed3
dev <dev@dev.myguest.virtualbox.org>
2024-05-11 14:42:14 +0200

    add ts ignore for stats js

--- src/main.ts
+++ src/main.ts
@@ -187,4 +187,5 @@ const main = async () => {
 main();
 
 // stats js
+// @ts-ignore
 (function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='https://mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
